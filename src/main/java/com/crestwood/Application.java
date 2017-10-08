0ac643529b91ab9a01885aeeb0a047add5f3dc71
rysav <ryssavage@byu.edu>
2017-10-07 20:39:35 -0600

    mvn executable

--- src/main/java/com/crestwood/Application.java
+++ src/main/java/com/crestwood/Application.java
@@ -22,18 +22,5 @@ public class Application extends SpringBootServletInitializer {
 
 
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
 }
